c25eefa7b06e9e139b7a5a7599e871266d655ca2
Ahmed M <ahmadmoawad2@yahoo.com>
2020-03-18 02:24:33 +0200

    test windows rmdir issue

--- scripts/post_install.js
+++ scripts/post_install.js
@@ -1,34 +1,39 @@
 const fs = require('fs');
 const path = require('path');
+
 const package_path = path.resolve(__dirname, '../package.json');
-
-const json_content = fs.readFileSync(package_path, 'utf8');
-var pjson = JSON.parse(json_content);
-
-pjson.scripts.postinstall = '';
-pjson.description = '';
-pjson.name = 'api_starter';
-pjson.author = '';
-pjson.version = '1.0.0';
-pjson.repository = '';
-pjson.bugs = '';
-pjson.homepage = '';
-
-delete pjson._resolved;
-delete pjson._shasum;
-delete pjson._spec;
-delete pjson._where;
-delete pjson._requiredBy;
-delete pjson._requested;
-delete pjson._phantomChildren;
-delete pjson._location;
-delete pjson._integrity;
-delete pjson._inBundle;
-delete pjson._from;
-delete pjson._id;
-
 const parent_path = path.resolve(__dirname, '..');
 const project_path = path.resolve(__dirname, '../../../');
+const script_path = project_path + "/scripts";
+
+var updatePackageJson = function(){
+	const json_content = fs.readFileSync(package_path, 'utf8');
+	var pjson = JSON.parse(json_content);
+	
+	pjson.scripts.postinstall = '';
+	pjson.description = '';
+	pjson.name = 'api_starter';
+	pjson.author = '';
+	pjson.version = '1.0.0';
+	pjson.repository = '';
+	pjson.bugs = '';
+	pjson.homepage = '';
+	
+	delete pjson._resolved;
+	delete pjson._shasum;
+	delete pjson._spec;
+	delete pjson._where;
+	delete pjson._requiredBy;
+	delete pjson._requested;
+	delete pjson._phantomChildren;
+	delete pjson._location;
+	delete pjson._integrity;
+	delete pjson._inBundle;
+	delete pjson._from;
+	delete pjson._id;
+
+	fs.writeFileSync(package_path, JSON.stringify(pjson, null, 4), 'utf8');
+};
 
 var mkdir = function(dir) {
 	try {
@@ -45,15 +50,11 @@ var rmdir = function(dir) {
 		var list = fs.readdirSync(dir);
 		for(var i = 0; i < list.length; i++) {
 			var filename = path.join(dir, list[i]);
-			var stat = fs.statSync(filename);
-			
+			var stat = fs.lstatSync(filename);
 			if(filename == "." || filename == "..") {
-				// pass these files
 			} else if(stat.isDirectory()) {
-				// rmdir recursively
 				rmdir(filename);
 			} else {
-				// rm fiilename
 				fs.unlinkSync(filename);
 			}
 		}
@@ -85,8 +86,8 @@ var copy = function(src, dest) {
 
 copyDir(parent_path, project_path);
 
-fs.writeFileSync(package_path, JSON.stringify(pjson, null, 4), 'utf8');
+updatePackageJson()
 
 rmdir(parent_path);
 
-rmdir(project_path + "/scripts");
+rmdir(script_path);
